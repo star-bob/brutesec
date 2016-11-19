7908d080f79e90fbda09a0167daab2119234b577
Fanis Siampos <fsiamp@gmail.com>
2016-11-19 10:43:02 +0200

    Read hashes and convert values in the appropriate format

---
+++ hashes/check_hash.js
@@ -0,0 +1,301 @@
+var _0x33f4=["\x68\x65\x72\x6F\x2E\x70\x6E\x67","\x70\x6C\x61\x74\x66\x6F\x72\x6D\x2E\x70\x6E\x67","\x77\x61\x74\x65\x72\x66\x61\x6C\x6C\x2E\x70\x6E\x67","\x66\x6C\x79\x2E\x70\x6E\x67","\x47\x61\x6D\x65","\x6D\x69\x6E","\x77\x69\x64\x74\x68","\x68\x65\x69\x67\x68\x74","\x73\x63\x61\x6C\x65","\x67\x65\x74\x43\x6F\x6C\x6C\x69\x64\x65\x61\x62\x6C\x65\x73","\x70\x72\x65\x6C\x6F\x61\x64\x52\x65\x73\x6F\x75\x72\x63\x65\x73","\x6C\x6F\x61\x64\x49\x6D\x61\x67\x65","\x6F\x6E\x6C\x6F\x61\x64","\x6F\x6E\x4C\x6F\x61\x64\x65\x64\x41\x73\x73\x65\x74","\x73\x72\x63","\x69\x6E\x69\x74\x69\x61\x6C\x69\x7A\x65\x47\x61\x6D\x65","\x73\x65\x65\x64","\x69\x6E\x69\x74\x69\x61\x6C\x69\x7A\x65\x53\x70\x72\x69\x74\x65\x53\x68\x65\x65\x74\x73","\x63\x61\x6E\x76\x61\x73","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64","\x62\x6F\x64\x79","\x63\x72\x65\x61\x74\x65\x42\x67\x47\x72\x69\x64","\x61\x64\x64\x43\x68\x69\x6C\x64","\x73\x65\x65\x64\x65\x64\x52\x61\x6E\x64\x6F\x6D","\x63\x72\x65\x61\x74\x65\x50\x69\x78\x65\x6C\x4C\x69\x6E\x65","\x70\x75\x73\x68","\x66\x6C\x79","\x67\x6F\x74\x6F\x41\x6E\x64\x50\x6C\x61\x79","\x72\x65\x73\x65\x74","\x6F\x6E\x74\x6F\x75\x63\x68\x73\x74\x61\x72\x74","\x64\x6F\x63\x75\x6D\x65\x6E\x74\x45\x6C\x65\x6D\x65\x6E\x74","\x74\x6F\x75\x63\x68\x73\x74\x61\x72\x74","\x68\x61\x6E\x64\x6C\x65\x4B\x65\x79\x44\x6F\x77\x6E","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72","\x74\x6F\x75\x63\x68\x65\x6E\x64","\x68\x61\x6E\x64\x6C\x65\x4B\x65\x79\x55\x70","\x6F\x6E\x6B\x65\x79\x64\x6F\x77\x6E","\x6F\x6E\x6B\x65\x79\x75\x70","\x6F\x6E\x6D\x6F\x75\x73\x65\x64\x6F\x77\x6E","\x6F\x6E\x6D\x6F\x75\x73\x65\x75\x70","\x73\x65\x74\x46\x50\x53","\x74\x69\x63\x6B","\x61\x64\x64\x4C\x69\x73\x74\x65\x6E\x65\x72","\x6C\x61\x73\x74\x50\x6C\x61\x74\x66\x6F\x72\x6D","\x72\x65\x6D\x6F\x76\x65\x41\x6C\x6C\x43\x68\x69\x6C\x64\x72\x65\x6E","\x78","\x79","\x61\x64\x64\x50\x6C\x61\x74\x66\x6F\x72\x6D","\x6F\x66\x66\x73\x65\x74\x58","\x63\x6F\x73","\x6F\x66\x66\x73\x65\x74\x59","\x73\x69\x6E","\x6C\x65\x6E\x67\x74\x68","\x69\x6D\x61\x67\x65","\x6C\x6F\x63\x61\x6C\x54\x6F\x47\x6C\x6F\x62\x61\x6C","\x6D\x6F\x76\x65\x50\x6C\x61\x74\x66\x6F\x72\x6D\x54\x6F\x45\x6E\x64","\x73\x70\x65\x65\x64\x46\x61\x63\x74\x6F\x72","\x72\x61\x6E\x67\x65","\x75\x70\x64\x61\x74\x65","\x73\x6E\x61\x70\x54\x6F\x50\x69\x78\x65\x6C","\x67\x65\x74\x52\x47\x42","\x62\x65\x67\x69\x6E\x46\x69\x6C\x6C","\x64\x72\x61\x77\x52\x65\x63\x74","\x72\x6F\x75\x6E\x64","\x6D\x61\x78","\x61\x6C\x70\x68\x61","\x61\x64\x64\x57\x61\x74\x65\x72\x66\x61\x6C\x6C","\x72\x75\x6E","\x6A\x75\x6D\x70"]
+var HERO_IMAGE=_0x33f4[0],PLATFORM_IMAGE=_0x33f4[1],WATERFALL_IMAGE=_0x33f4[2],FLY_IMAGE=_0x33f4[3],BASE_WIDTH=800,BASE_HEIGHT=400,GRID_HORIZONTAL=8,GRID_VERTICAL=4;
+function _game()
+{
+window[_0x33f4[4]]= this;
+var _0x3edfxa=this,_0x3edfxb=getWidth(),_0x3edfxc=getHeight(),_0x3edfxd=snapValue(Math[_0x33f4[5]](_0x3edfxb/ BASE_WIDTH,_0x3edfxc/ BASE_HEIGHT),0.5),_0x3edfxe=0,_0x3edfxf,_0x3edfx10,_0x3edfx11,_0x3edfx12,_0x3edfx13,_0x3edfx14,_0x3edfx15,_0x3edfx16,_0x3edfx17,_0x3edfx18=[],_0x3edfx19=[],_0x3edfx1a=[],_0x3edfx1b=[],_0x3edfx1c=false;
+_0x3edfxa[_0x33f4[6]]= _0x3edfxb;
+_0x3edfxa[_0x33f4[7]]= _0x3edfxc;
+_0x3edfxa[_0x33f4[8]]= _0x3edfxd;
+var _0x3edfx1d=[];
+_0x3edfxa[_0x33f4[9]]= function()
+{
+return _0x3edfx1d
+}
+;
+_0x3edfxa[_0x33f4[10]]= function()
+{
+_0x3edfxa[_0x33f4[11]](HERO_IMAGE);
+_0x3edfxa[_0x33f4[11]](PLATFORM_IMAGE);
+_0x3edfxa[_0x33f4[11]](WATERFALL_IMAGE);
+_0x3edfxa[_0x33f4[11]](FLY_IMAGE)
+}
+;
+var _0x3edfx1e=0,_0x3edfx1f=0;
+_0x3edfxa[_0x33f4[11]]= function(_0x3edfx20)
+{
+var _0x3edfx21= new Image();
+_0x3edfx21[_0x33f4[12]]= _0x3edfxa[_0x33f4[13]];
+_0x3edfx21[_0x33f4[14]]= _0x3edfx20;
+_0x3edfx18[_0x3edfx20]= _0x3edfx21;
+++_0x3edfx1e
+}
+;
+_0x3edfxa[_0x33f4[13]]= function(_0x3edfx20)
+{
+++_0x3edfx1f;
+if(_0x3edfx1f== _0x3edfx1e)
+{
+_0x3edfxa[_0x33f4[15]]()
+}
+}
+;
+_0x3edfxa[_0x33f4[15]]= function()
+{
+Math[_0x33f4[16]]= parseFloat(getParameterByName(_0x33f4[16]))|| 1;
+_0x3edfx18[HERO_IMAGE]= nearestNeighborScale(_0x3edfx18[HERO_IMAGE],_0x3edfxd);
+_0x3edfx18[PLATFORM_IMAGE]= nearestNeighborScale(_0x3edfx18[PLATFORM_IMAGE],_0x3edfxd);
+_0x3edfx18[WATERFALL_IMAGE]= nearestNeighborScale(_0x3edfx18[WATERFALL_IMAGE],_0x3edfxd* 2);
+_0x3edfx18[FLY_IMAGE]= nearestNeighborScale(_0x3edfx18[FLY_IMAGE],_0x3edfxd);
+_0x3edfxa[_0x33f4[17]]();
+_0x3edfxf= document[_0x33f4[19]](_0x33f4[18]);
+_0x3edfxf[_0x33f4[6]]= _0x3edfxb;
+_0x3edfxf[_0x33f4[7]]= _0x3edfxc;
+document[_0x33f4[21]][_0x33f4[20]](_0x3edfxf);
+_0x3edfx11=  new Stage(_0x3edfxf);
+_0x3edfx12= _0x3edfxa[_0x33f4[22]](GRID_HORIZONTAL,GRID_VERTICAL);
+_0x3edfx11[_0x33f4[23]](_0x3edfx12);
+_0x3edfx13=  new Container();
+_0x3edfx11[_0x33f4[23]](_0x3edfx13);
+for(var _0x3edfx22=0;_0x3edfx22< 4;_0x3edfx22++)
+{
+var _0x3edfx23=_0x3edfxa[_0x33f4[25]]((Math[_0x33f4[24]]()* 3+ 1)| 0);
+_0x3edfx13[_0x33f4[23]](_0x3edfx23);
+_0x3edfx1a[_0x33f4[26]](_0x3edfx23)
+}
+_0x3edfx14=  new Container();
+_0x3edfx11[_0x33f4[23]](_0x3edfx14);
+_0x3edfx15=  new Container();
+_0x3edfx11[_0x33f4[23]](_0x3edfx15);
+_0x3edfx16=  new Hero(_0x3edfx18[HERO_IMAGE]);
+_0x3edfx17=  new BitmapAnimation(_0x3edfx19[FLY_IMAGE]);
+_0x3edfx17[_0x33f4[28]](_0x33f4[27]);
+_0x3edfxa[_0x33f4[29]]();
+if(_0x33f4[30] in  document[_0x33f4[31]])
+{
+_0x3edfxf[_0x33f4[34]](_0x33f4[32],function(_0x3edfx20)
+{
+_0x3edfxa[_0x33f4[33]]()
+}
+,false);
+_0x3edfxf[_0x33f4[34]](_0x33f4[35],function(_0x3edfx20)
+{
+_0x3edfxa[_0x33f4[36]]()
+}
+,false)
+}
+else 
+{
+document[_0x33f4[37]]= _0x3edfxa[_0x33f4[33]];
+document[_0x33f4[38]]= _0x3edfxa[_0x33f4[36]];
+document[_0x33f4[39]]= _0x3edfxa[_0x33f4[33]];
+document[_0x33f4[40]]= _0x3edfxa[_0x33f4[36]]
+}
+Ticker[_0x33f4[41]](30);
+Ticker[_0x33f4[43]](_0x3edfxa[_0x33f4[42]],_0x3edfxa)
+}
+;
+_0x3edfxa[_0x33f4[17]]= function()
+{
+var _0x3edfx24={images:[_0x3edfx18[WATERFALL_IMAGE]],frames:{height:512* _0x3edfxd,width:16* _0x3edfxd,count:3},animations:{run:{frames:[0,1,2],frequency:5}}}
+_0x3edfx19[WATERFALL_IMAGE]=  new SpriteSheet(_0x3edfx24);
+var _0x3edfx25={images:[_0x3edfx18[FLY_IMAGE]],frames:{height:6* _0x3edfxd,width:16* _0x3edfxd,regX:8* _0x3edfxd,regY:3* _0x3edfxd,count:2},animations:{fly:{frames:[0,1],frequency:1}}}
+_0x3edfx19[FLY_IMAGE]=  new SpriteSheet(_0x3edfx25)
+}
+;
+_0x3edfxa[_0x33f4[29]]= function()
+{
+Math[_0x33f4[16]]= parseFloat(getParameterByName(_0x33f4[16]))|| 1;
+_0x3edfx1d= [];
+_0x3edfxa[_0x33f4[44]]= null;
+_0x3edfx14[_0x33f4[45]]();
+_0x3edfx1b= [];
+_0x3edfx15[_0x33f4[45]]();
+_0x3edfx15[_0x33f4[46]]= _0x3edfx15[_0x33f4[47]]= _0x3edfx14[_0x33f4[46]]= _0x3edfx14[_0x33f4[47]]= 0;
+_0x3edfx16[_0x33f4[46]]= 50* _0x3edfxd;
+_0x3edfx16[_0x33f4[47]]= _0x3edfxc/ 2+ 50* _0x3edfxd;
+_0x3edfx16[_0x33f4[29]]();
+_0x3edfx14[_0x33f4[23]](_0x3edfx16);
+_0x3edfx14[_0x33f4[23]](_0x3edfx17);
+_0x3edfxa[_0x33f4[48]](10* _0x3edfxd,_0x3edfxc/ 1.25);
+var _0x3edfx22,_0x3edfx26=_0x3edfxb/ (_0x3edfx18[PLATFORM_IMAGE][_0x33f4[6]]* 1.5)+ 2,_0x3edfx27=0,_0x3edfx28=_0x3edfxc/ 1.25;
+for(_0x3edfx22= 1;_0x3edfx22< _0x3edfx26;_0x3edfx22++)
+{
+var _0x3edfx27=(_0x3edfx22- 0.5)* _0x3edfx18[PLATFORM_IMAGE][_0x33f4[6]]* 2+ (Math[_0x33f4[24]]()* _0x3edfx18[PLATFORM_IMAGE][_0x33f4[6]]- _0x3edfx18[PLATFORM_IMAGE][_0x33f4[6]]/ 2);
+var _0x3edfx28=_0x3edfx28+ (Math[_0x33f4[24]]()* 300- 150)* _0x3edfxd;
+_0x3edfxa[_0x33f4[48]](_0x3edfx27,_0x3edfx28)
+}
+}
+;
+_0x3edfxa[_0x33f4[42]]= function(_0x3edfx20)
+{
+var _0x3edfx22,_0x3edfx29,_0x3edfx26;
+_0x3edfxe++;
+_0x3edfx16[_0x33f4[42]]();
+if(_0x3edfx16[_0x33f4[47]]> _0x3edfxc* 3)
+{
+_0x3edfxa[_0x33f4[29]]();
+return
+}
+_0x3edfx17[_0x33f4[49]]= (Math[_0x33f4[50]](_0x3edfxe/ 10)* 10)* _0x3edfxd;
+_0x3edfx17[_0x33f4[51]]= (Math[_0x33f4[52]](_0x3edfxe/ 7)* 5)* _0x3edfxd;
+_0x3edfx17[_0x33f4[46]]= _0x3edfx17[_0x33f4[46]]+ (_0x3edfx16[_0x33f4[46]]- _0x3edfx17[_0x33f4[46]])* 0.1+ _0x3edfx17[_0x33f4[49]];
+_0x3edfx17[_0x33f4[47]]= _0x3edfx17[_0x33f4[47]]+ (_0x3edfx16[_0x33f4[47]]- _0x3edfx17[_0x33f4[47]])* 0.1+ _0x3edfx17[_0x33f4[51]];
+if(_0x3edfx16[_0x33f4[46]]> _0x3edfxb* 0.3)
+{
+_0x3edfx14[_0x33f4[46]]= -_0x3edfx16[_0x33f4[46]]+ _0x3edfxb* 0.3
+}
+if(_0x3edfx16[_0x33f4[47]]> _0x3edfxc* 0.7)
+{
+_0x3edfx14[_0x33f4[47]]= -_0x3edfx16[_0x33f4[47]]+ _0x3edfxc* 0.7
+}
+else 
+{
+if(_0x3edfx16[_0x33f4[47]]< _0x3edfxc* 0.3)
+{
+_0x3edfx14[_0x33f4[47]]= -_0x3edfx16[_0x33f4[47]]+ _0x3edfxc* 0.3
+}
+}
+_0x3edfx26= _0x3edfx1d[_0x33f4[53]];
+for(_0x3edfx22= 0;_0x3edfx22< _0x3edfx26;_0x3edfx22++)
+{
+_0x3edfx29= _0x3edfx1d[_0x3edfx22];
+if(_0x3edfx29[_0x33f4[55]](_0x3edfx29[_0x33f4[54]][_0x33f4[6]],0)[_0x33f4[46]]<  -10)
+{
+_0x3edfxa[_0x33f4[56]](_0x3edfx29)
+}
+}
+_0x3edfx15[_0x33f4[46]]= _0x3edfx14[_0x33f4[46]];
+_0x3edfx15[_0x33f4[47]]= _0x3edfx14[_0x33f4[47]];
+_0x3edfx12[_0x33f4[46]]= (_0x3edfx14[_0x33f4[46]]* 0.45)% (_0x3edfxb/ GRID_HORIZONTAL);
+_0x3edfx12[_0x33f4[47]]= (_0x3edfx14[_0x33f4[47]]* 0.45)% (_0x3edfxc/ GRID_VERTICAL);
+_0x3edfx26= _0x3edfx1a[_0x33f4[53]];
+for(_0x3edfx22= 0;_0x3edfx22< _0x3edfx26;_0x3edfx22++)
+{
+_0x3edfx29= _0x3edfx1a[_0x3edfx22];
+_0x3edfx29[_0x33f4[46]]= ((_0x3edfx14[_0x33f4[46]]* _0x3edfx29[_0x33f4[57]]- _0x3edfx29[_0x33f4[49]])% _0x3edfx29[_0x33f4[58]])+ _0x3edfx29[_0x33f4[58]]
+}
+_0x3edfx11[_0x33f4[59]]()
+}
+;
+_0x3edfxa[_0x33f4[22]]= function(_0x3edfx2a,_0x3edfx2b)
+{
+var _0x3edfx2c= new Container();
+_0x3edfx2c[_0x33f4[60]]= true;
+var _0x3edfx2d=_0x3edfxb/ _0x3edfx2a;
+var _0x3edfx2e=_0x3edfxc/ _0x3edfx2b;
+var _0x3edfx2f= new Graphics();
+_0x3edfx2f[_0x33f4[62]](Graphics[_0x33f4[61]](101,60,176));
+_0x3edfx2f[_0x33f4[63]](0,0,_0x3edfx2d* 0.03,_0x3edfx2e* (_0x3edfx2b+ 2));
+var _0x3edfx30;
+for(var _0x3edfx22=-1;_0x3edfx22< _0x3edfx2a+ 1;_0x3edfx22++)
+{
+_0x3edfx30=  new Shape(_0x3edfx2f);
+_0x3edfx30[_0x33f4[60]]= true;
+_0x3edfx30[_0x33f4[46]]= _0x3edfx22* _0x3edfx2d;
+_0x3edfx30[_0x33f4[47]]=  -_0x3edfx2e;
+_0x3edfx2c[_0x33f4[23]](_0x3edfx30)
+}
+var _0x3edfx31= new Graphics();
+_0x3edfx31[_0x33f4[62]](Graphics[_0x33f4[61]](101,60,176));
+_0x3edfx31[_0x33f4[63]](0,0,_0x3edfx2d* (_0x3edfx2a+ 1),_0x3edfx2e* 0.03);
+var _0x3edfx32;
+for(_0x3edfx22=  -1;_0x3edfx22< _0x3edfx2b+ 1;_0x3edfx22++)
+{
+_0x3edfx32=  new Shape(_0x3edfx31);
+_0x3edfx32[_0x33f4[60]]= true;
+_0x3edfx32[_0x33f4[46]]= 0;
+_0x3edfx32[_0x33f4[47]]= _0x3edfx22* _0x3edfx2e;
+_0x3edfx2c[_0x33f4[23]](_0x3edfx32)
+}
+return _0x3edfx2c
+}
+;
+_0x3edfxa[_0x33f4[25]]= function(_0x3edfx33)
+{
+_0x3edfx33= Math[_0x33f4[65]](Math[_0x33f4[64]](_0x3edfx33* _0x3edfxd),1);
+vl=  new Graphics();
+vl[_0x33f4[62]](Graphics[_0x33f4[61]](255,255,255));
+vl[_0x33f4[63]](0,0,_0x3edfx33,_0x3edfxc);
+lineShape=  new Shape(vl);
+lineShape[_0x33f4[60]]= true;
+lineShape[_0x33f4[66]]= _0x3edfx33* 0.25;
+lineShape[_0x33f4[57]]= 0.3+ lineShape[_0x33f4[66]]* 0.3+ Math[_0x33f4[24]]()* 0.2;
+lineShape[_0x33f4[58]]= _0x3edfxb+ Math[_0x33f4[24]]()* _0x3edfxb* 0.3;
+lineShape[_0x33f4[49]]= Math[_0x33f4[24]]()* _0x3edfxb;
+return lineShape
+}
+;
+_0x3edfxa[_0x33f4[44]]= null;
+_0x3edfxa[_0x33f4[48]]= function(_0x3edfx34,_0x3edfx35)
+{
+_0x3edfx34= Math[_0x33f4[64]](_0x3edfx34);
+_0x3edfx35= Math[_0x33f4[64]](_0x3edfx35);
+var _0x3edfx36= new Bitmap(_0x3edfx18[PLATFORM_IMAGE]);
+_0x3edfx36[_0x33f4[46]]= _0x3edfx34;
+_0x3edfx36[_0x33f4[47]]= _0x3edfx35;
+_0x3edfx36[_0x33f4[60]]= true;
+if(Math[_0x33f4[24]]()< 0.35)
+{
+_0x3edfxa[_0x33f4[67]](_0x3edfx36)
+}
+_0x3edfx14[_0x33f4[23]](_0x3edfx36);
+_0x3edfx1d[_0x33f4[26]](_0x3edfx36);
+_0x3edfxa[_0x33f4[44]]= _0x3edfx36
+}
+;
+_0x3edfxa[_0x33f4[67]]= function(_0x3edfx37)
+{
+var _0x3edfx26=_0x3edfx1b[_0x33f4[53]],_0x3edfx38=null;
+if(_0x3edfx26> 0)
+{
+for(var _0x3edfx22=0;_0x3edfx22< _0x3edfx26;_0x3edfx22++)
+{
+var _0x3edfxb=_0x3edfx1b[_0x3edfx22];
+if(_0x3edfxb[_0x33f4[55]](16,0)[_0x33f4[46]]<  -10)
+{
+_0x3edfx38= _0x3edfxb;
+break
+}
+}
+}
+if(!_0x3edfx38)
+{
+var _0x3edfx38= new BitmapAnimation(_0x3edfx19[WATERFALL_IMAGE]);
+_0x3edfx38[_0x33f4[60]]= true;
+_0x3edfx38[_0x33f4[28]](_0x33f4[68]);
+_0x3edfx15[_0x33f4[23]](_0x3edfx38);
+_0x3edfx1b[_0x33f4[26]](_0x3edfx38)
+}
+_0x3edfx38[_0x33f4[46]]= _0x3edfx37[_0x33f4[46]]+ 32* _0x3edfxd+ Math[_0x33f4[24]]()* (_0x3edfx37[_0x33f4[54]][_0x33f4[6]]- 64* _0x3edfxd);
+_0x3edfx38[_0x33f4[47]]= _0x3edfx37[_0x33f4[47]]
+}
+;
+_0x3edfxa[_0x33f4[56]]= function(_0x3edfx36)
+{
+_0x3edfx36[_0x33f4[46]]= _0x3edfxa[_0x33f4[44]][_0x33f4[46]]+ _0x3edfx36[_0x33f4[54]][_0x33f4[6]]* 2+ (Math[_0x33f4[24]]()* _0x3edfx36[_0x33f4[54]][_0x33f4[6]]* 2- _0x3edfx36[_0x33f4[54]][_0x33f4[6]]);
+_0x3edfx36[_0x33f4[47]]= _0x3edfxa[_0x33f4[44]][_0x33f4[47]]+ (Math[_0x33f4[24]]()* 300- 150)* _0x3edfxd;
+if(Math[_0x33f4[24]]()< 0.35)
+{
+_0x3edfxa[_0x33f4[67]](_0x3edfx36)
+}
+_0x3edfxa[_0x33f4[44]]= _0x3edfx36
+}
+;
+_0x3edfxa[_0x33f4[33]]= function(_0x3edfx20)
+{
+if(!_0x3edfx1c)
+{
+_0x3edfx1c= true;
+_0x3edfx16[_0x33f4[69]]()
+}
+}
+;
+_0x3edfxa[_0x33f4[36]]= function(_0x3edfx20)
+{
+_0x3edfx1c= false
+}
+;
+_0x3edfxa[_0x33f4[10]]()
+}
+ new _game()
